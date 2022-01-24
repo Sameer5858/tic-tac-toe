f986786323cdace261a9510887c8e4a395efd6cd
Sameer5858 <96295049+Sameer5858@users.noreply.github.com>
2022-01-24 18:17:15 +0530

    css done

--- script.js
+++ script.js
@@ -42,53 +42,66 @@ const gameBoard = (() => {
 })();
 const displayController = (() => {
   const display = document.querySelector(".display-message");
+  // function to show players turn
   function turnMessage(player) {
-    display.textContent = `It's ${player.name}'s turn`;
+    display.textContent = `${player.name}'s turn.`;
   }
-
+  // function to display win message
   function winMessage(player) {
     display.textContent = `${player.name} has won this match!!!`;
   }
+
   const cells = document.querySelectorAll(".cell");
+  // event listener for click in cells to show the signs
   cells.forEach((cell) => {
     cell.addEventListener("click", () => {
       if (cell.textContent === "" && gameFlow.gameOver === false) {
         if (gameFlow.player1.isActive) {
           cell.textContent = gameFlow.player1.sign;
+          cell.classList.add("taken");
           gameBoard.setCell(`${cell.getAttribute("data-index")}`, "X");
           gameFlow.player1.toggle();
           gameFlow.player2.toggle();
           gameFlow.round++;
           turnMessage(gameFlow.player2);
+          if (gameFlow.round === 9) {
+            display.textContent = "It's a tie.";
+          }
           gameFlow.winCheck(gameFlow.player1);
         } else if (gameFlow.player2.isActive) {
           cell.textContent = gameFlow.player2.sign;
+          cell.classList.add("taken");
           gameBoard.setCell(`${cell.getAttribute("data-index")}`, "O");
           gameFlow.player2.toggle();
           gameFlow.player1.toggle();
           gameFlow.round++;
           turnMessage(gameFlow.player1);
+          if (gameFlow.round === 9) {
+            display.textContent = "It's a tie.";
+          }
           gameFlow.winCheck(gameFlow.player2);
         }
       }
     });
   });
   const restartButton = document.getElementById("restart");
+  // event listener for restart button to clear everything and start from start
   restartButton.addEventListener("click", () => {
     gameFlow.reset();
     cells.forEach((cell) => {
       cell.textContent = "";
+      cell.classList.remove("taken");
     });
   });
   return { winMessage, turnMessage };
 })();
 const gameFlow = (() => {
-  const player1 = playerFactory("Player 1", "X", true);
-  const player2 = playerFactory("Player 2", "O", false);
+  const player1 = playerFactory("Player X", "X", true);
+  const player2 = playerFactory("Player O", "O", false);
   let round = 0;
   let gameOver = false;
   displayController.turnMessage(player1);
-
+  // function to reset to be called by restart button
   function reset() {
     gameFlow.round = 0;
     gameFlow.gameOver = false;
@@ -101,6 +114,7 @@ const gameFlow = (() => {
       gameFlow.player2.toggle();
     }
   }
+  // function to check win to be called by click on cells
   function winCheck(player) {
     if (
       (gameBoard.getBoard[0] === gameBoard.getBoard[1] &&
