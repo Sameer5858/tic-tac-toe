d43f380ccc7d95cea076abaa1ff73ee70619eda6
sameer5858 <www.md.sameerkhan5858@gmail.com>
2022-01-24 14:54:30 +0530

    win logic added

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ const playerFactory = (name, sign, isActive) => {
     },
   };
 };
-
 const gameBoard = (() => {
   let board = new Array(9).fill("");
   function setCell(index, sign) {
@@ -41,11 +40,97 @@ const gameBoard = (() => {
     resetBoard,
   };
 })();
-const displayCOntroller = (() => {
+const displayController = (() => {
+  const display = document.querySelector(".display-message");
+  function turnMessage(player) {
+    display.textContent = `It's ${player.name}'s turn`;
+  }
+
+  function winMessage(player) {
+    display.textContent = `${player.name} has won this match!!!`;
+  }
   const cells = document.querySelectorAll(".cell");
   cells.forEach((cell) => {
     cell.addEventListener("click", () => {
-      console.log("hello");
+      if (cell.textContent === "" && gameFlow.gameOver === false) {
+        if (gameFlow.player1.isActive) {
+          cell.textContent = gameFlow.player1.sign;
+          gameBoard.setCell(`${cell.getAttribute("data-index")}`, "X");
+          gameFlow.player1.toggle();
+          gameFlow.player2.toggle();
+          gameFlow.round++;
+          turnMessage(gameFlow.player2);
+          gameFlow.winCheck(gameFlow.player1);
+        } else if (gameFlow.player2.isActive) {
+          cell.textContent = gameFlow.player2.sign;
+          gameBoard.setCell(`${cell.getAttribute("data-index")}`, "O");
+          gameFlow.player2.toggle();
+          gameFlow.player1.toggle();
+          gameFlow.round++;
+          turnMessage(gameFlow.player1);
+          gameFlow.winCheck(gameFlow.player2);
+        }
+      }
     });
   });
+  const restartButton = document.getElementById("restart");
+  restartButton.addEventListener("click", () => {
+    gameFlow.reset();
+    cells.forEach((cell) => {
+      cell.textContent = "";
+    });
+  });
+  return { winMessage, turnMessage };
+})();
+const gameFlow = (() => {
+  const player1 = playerFactory("Player 1", "X", true);
+  const player2 = playerFactory("Player 2", "O", false);
+  let round = 0;
+  let gameOver = false;
+  displayController.turnMessage(player1);
+
+  function reset() {
+    gameFlow.round = 0;
+    gameFlow.gameOver = false;
+    gameBoard.resetBoard();
+    displayController.turnMessage(player1);
+    if (gameFlow.player1.isActive === false) {
+      gameFlow.player1.toggle();
+    }
+    if (gameFlow.player2.isActive === true) {
+      gameFlow.player2.toggle();
+    }
+  }
+  function winCheck(player) {
+    if (
+      (gameBoard.getBoard[0] === gameBoard.getBoard[1] &&
+        gameBoard.getBoard[1] === gameBoard.getBoard[2] &&
+        gameBoard.getBoard[0] !== "") ||
+      (gameBoard.getBoard[3] === gameBoard.getBoard[4] &&
+        gameBoard.getBoard[4] === gameBoard.getBoard[5] &&
+        gameBoard.getBoard[3] !== "") ||
+      (gameBoard.getBoard[6] === gameBoard.getBoard[7] &&
+        gameBoard.getBoard[7] === gameBoard.getBoard[8] &&
+        gameBoard.getBoard[6] !== "") ||
+      (gameBoard.getBoard[0] === gameBoard.getBoard[3] &&
+        gameBoard.getBoard[3] === gameBoard.getBoard[6] &&
+        gameBoard.getBoard[0] !== "") ||
+      (gameBoard.getBoard[1] === gameBoard.getBoard[4] &&
+        gameBoard.getBoard[4] === gameBoard.getBoard[7] &&
+        gameBoard.getBoard[1] !== "") ||
+      (gameBoard.getBoard[2] === gameBoard.getBoard[5] &&
+        gameBoard.getBoard[5] === gameBoard.getBoard[8] &&
+        gameBoard.getBoard[2] !== "") ||
+      (gameBoard.getBoard[0] === gameBoard.getBoard[4] &&
+        gameBoard.getBoard[4] === gameBoard.getBoard[8] &&
+        gameBoard.getBoard[0] !== "") ||
+      (gameBoard.getBoard[2] === gameBoard.getBoard[4] &&
+        gameBoard.getBoard[4] === gameBoard.getBoard[6] &&
+        gameBoard.getBoard[2] !== "")
+    ) {
+      displayController.winMessage(player);
+      gameFlow.gameOver = true;
+    }
+  }
+  return { player1, player2, round, reset, winCheck, gameOver };
 })();
